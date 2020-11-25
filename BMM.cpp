c3e2ec85e2ad0c8c269761f9ef3ec793cf398c3f
mahzoun <mohammad.mahzoun@gmail.com>
2020-11-25 09:53:18 +0100

    Generate ciphertexts

--- BMM.cpp
+++ BMM.cpp
@@ -6,39 +6,49 @@
 
 uint32_t * BMM::teta(uint32_t x[BLOCK_SIZE])
 {
-    uint32_t y[BLOCK_SIZE] = {x[0] ^ x[1] ^ x[3], x[0] ^ x[1] ^ x[2], x[1] ^ x[2] ^ x[3], x[0] ^ x[2] ^ x[3]};
+    uint32_t *y = new uint32_t[BLOCK_SIZE];
+    y[0] = x[0] ^ x[1] ^ x[3];
+    y[1] = x[0] ^ x[1] ^ x[2];
+    y[2] = x[1] ^ x[2] ^ x[3];
+    y[3] = x[0] ^ x[2] ^ x[3];
     return y;
 }
 
 uint32_t * BMM::eta(uint32_t x[BLOCK_SIZE])
 {
-    uint32_t y[BLOCK_SIZE] = {x[0] ^ ((1&x[0])*delta) , x[1], x[2], x[3] ^ ((1&x[3])*delta)};
+    uint32_t *y = new uint32_t[BLOCK_SIZE];
+    y[0] = x[0] ^ ((1&x[0])*delta);
+    y[1] = x[1];
+    y[2] = x[2];
+    y[3] = x[3] ^ ((1&x[3])*delta);
+    return y;
 }
 
 uint32_t * BMM::gamma(uint32_t x[BLOCK_SIZE])
 {
-    uint32_t y[BLOCK_SIZE];
+    uint32_t *y = new uint32_t[BLOCK_SIZE];
     for (uint8_t i = 0; i < BLOCK_SIZE; i++){
         if (x[i] == ~0x0)
             y[i] = x[i];
         else
             y[i] = x[i] * G[i];
     }
+    return y;
 }
 
 uint32_t * BMM::sigma(uint32_t x[BLOCK_SIZE], uint32_t k[BLOCK_SIZE], uint8_t J)
 {
-    uint32_t y[BLOCK_SIZE];
-    for (uint8_t i = 0; i < BLOCK_SIZE; i++){
-        uint32_t k_iJ = k[(i+J)%4 ^ ((1<<J) * B)];
+    uint32_t *y = new uint32_t[BLOCK_SIZE];
+    for (int i = 0; i < BLOCK_SIZE; i++){
+        uint32_t k_iJ = k[(i+J)%4] ^ ((1<<J) * B);
         y[i] = x[i] ^ k_iJ;
     }
+    return y;
 }
 
 uint32_t * BMM::rho(uint32_t x[BLOCK_SIZE], uint32_t k[BLOCK_SIZE], uint8_t J)
 {
-    uint32_t *y;
-    y = sigma(x, k, J);
+    uint32_t *y = sigma(x, k, J);
     y = gamma(y);
     y = eta(y);
     y = teta(y);
--- main.cpp
+++ main.cpp
@@ -21,19 +21,31 @@ int main() {
     for(uint8_t i = 0; i < bmm.BLOCK_SIZE; i++)
         k[i] = rand();
 
+    for(int i = 0 ; i < (1<<17); i++){
+        C1[i] = new uint32_t[bmm.BLOCK_SIZE];
+        C2[i] = new uint32_t[bmm.BLOCK_SIZE];
+        C3[i] = new uint32_t[bmm.BLOCK_SIZE];
+        C4[i] = new uint32_t[bmm.BLOCK_SIZE];
+    }
+
     for(uint32_t i = 0; i < (1<<17); i++){
+        // Generate randome plaintexts
         P1[i][0] = rand();
         P2[i][0] = P1[i][0];
         P2[i][1] = ~0x0 ^ P1[i][1];
         P2[i][2] = P1[i][2];
         P2[i][3] = ~0x0 ^ P1[i][3];
+        // Generate encryption
         C1[i] = bmm.Enc(P1[i], k);
         C2[i] = bmm.Enc(P2[i], k);
+        C3[i][0] = C1[i][0];
+        C3[i][1] = C1[i][1];
+        C3[i][2] = C1[i][2] ^ ~0x0;
+        C3[i][3] = C1[i][3] ^ ~0x0;
+        C4[i][0] = C2[i][0];
+        C4[i][1] = C2[i][1];
+        C4[i][2] = C2[i][2] ^ ~0x0;
+        C4[i][3] = C2[i][3] ^ ~0x0;
     }
-
-//    for(uint8_t i = 0; i < bmm.BLOCK_SIZE; i++) {
-//        std::bitset<32> y(k[i]);
-//        std::cout << k[i] << " " << y << std::endl;
-//    }
     return 0;
 }
